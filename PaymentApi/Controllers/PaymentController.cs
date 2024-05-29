132e48586ae5f3c9b800295983775e98a5e2b58a
sureshkalyan1000 <suresheeeo44@gmail.com>
2024-05-29 23:57:38 +0530

    PaymentApi

---
+++ PaymentApi/Controllers/PaymentController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaymentApi.Models;
+
+namespace PaymentApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly PaymentDetailsContext _context;
+
+        public PaymentController(PaymentDetailsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Payment
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentDetails>>> GetPaymentDetails()
+        {
+            return await _context.PaymentDetails.ToListAsync();
+        }
+
+        // GET: api/Payment/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaymentDetails>> GetPaymentDetails(int id)
+        {
+            var paymentDetails = await _context.PaymentDetails.FindAsync(id);
+
+            if (paymentDetails == null)
+            {
+                return NotFound();
+            }
+
+            return paymentDetails;
+        }
+
+        // PUT: api/Payment/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentDetails(int id, PaymentDetails paymentDetails)
+        {
+            if (id != paymentDetails.PaymentDetailId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(paymentDetails).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentDetailsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Payment
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PaymentDetails>> PostPaymentDetails(PaymentDetails paymentDetails)
+        {
+            _context.PaymentDetails.Add(paymentDetails);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.PaymentDetails.ToListAsync());
+        }
+
+        // DELETE: api/Payment/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaymentDetails(int id)
+        {
+            var paymentDetails = await _context.PaymentDetails.FindAsync(id);
+            if (paymentDetails == null)
+            {
+                return NotFound();
+            }
+
+            _context.PaymentDetails.Remove(paymentDetails);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PaymentDetailsExists(int id)
+        {
+            return _context.PaymentDetails.Any(e => e.PaymentDetailId == id);
+        }
+    }
+}
---
+++ PaymentApi/Models/PaymentDetailsContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentApi.Models
+{
+    public class PaymentDetailsContext : DbContext
+    {
+        public PaymentDetailsContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+    }
+}
